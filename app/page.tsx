b1041db38dcc7fae7cdaa15eda0fb175064bb158
mimmiGenevieve <mimmisandgren@gmail.com>
2024-11-01 23:17:23 +0100

    More typescript fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 
 'use client'
-import React, { useState, useEffect,useRef, RefObject  } from "react";
+import React, { useState, useEffect,useRef  } from "react";
 import emailjs from "emailjs-com";
 
 interface Option {
