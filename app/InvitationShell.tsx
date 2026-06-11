313c4754de4dfeaa53a6076eae37e0357a8b7560
mimmiGenevieve <mimmisandgren@gmail.com>
2026-06-11 21:58:42 +0200

    Styling

--- app/InvitationShell.tsx
+++ app/InvitationShell.tsx
@@ -18,29 +18,31 @@ export default function InvitationShell({
     const pathPrefix = activePage === 'details' ? '' : 'rsvp'
     return (
         <div className="flex flex-col flex-1 items-center justify-center bg-linear-to-b from-mauve-900/10 to-mauve-950/90">
-            <main className="flex flex-1 w-full flex-col items-center py-20">
+            <main className="flex flex-1 w-full flex-col items-center mt-10 lg:mt-15">
                 <div className="flex w-full flex-col items-center text-shadow-lg bg-(--background)/60 p-[50px] pb-5">
-                    <h1 className="text-9xl font-bold ">The Halloweeners</h1>
-                    <p className="text-2xl mt-4">
+                    <h1 className="lg:text-9xl text-6xl font-bold">
+                        The Halloweeners
+                    </h1>
+                    <p className="lg:text-2xl text-xl mt-4">
                         An Invitation to the Eternal Night.
                     </p>
 
                     {isAuthenticated && (
-                        <div className="flex flex-row items-center justify-between mt-10 gap-10 text-2xl">
+                        <div className="flex flex-row items-center justify-between mt-10 gap-10 lg:text-2xl text-xl">
                             <Link
                                 href="/"
-                                className={`cursor-pointer ${
+                                className={
                                     activePage === 'details' ? 'underline' : ''
-                                }`}
+                                }
                             >
                                 Details
                             </Link>
                             |
                             <Link
                                 href="/rsvp"
-                                className={`cursor-pointer ${
+                                className={
                                     activePage === 'rsvp' ? 'underline' : ''
-                                }`}
+                                }
                             >
                                 RSVP
                             </Link>
@@ -48,11 +50,11 @@ export default function InvitationShell({
                     )}
                 </div>
                 {isAuthenticated ? (
-                    <div className="flex flex-col bg-(--background)/60 p-[50px] mt-30 gap-2 text-xl w-200 h-170 overflow-y-auto no-scrollbar">
+                    <div className="flex flex-col bg-(--background)/60 p-[50px] mt-10 lg:mt-20 gap-2 text-xl w-(100%) lg:w-200 overflow-y-auto no-scrollbar">
                         {children}
                     </div>
                 ) : (
-                    <div className="flex flex-col bg-(--background)/60 p-[50px] mt-30 gap-2 text-xl w-200 h-110 overflow-y-auto no-scrollbar">
+                    <div className="flex flex-col bg-(--background)/60 p-[50px] mt-10 lg:mt-20 gap-2 text-xl w-(100%) lg:w-200 lg:h-110 overflow-y-auto no-scrollbar">
                         <TokenAccessForm
                             nextPath={`/${pathPrefix}`}
                             authError={authError}
--- app/TokenAccessForm.tsx
+++ app/TokenAccessForm.tsx
@@ -12,13 +12,37 @@ export default function TokenAccessForm({
     authError,
 }: TokenAccessFormProps) {
     const [hasError, setHasError] = useState(authError === 'invalid_token')
+    const [loading, setIsLoading] = useState(false)
+    const [formData, setFormData] = useState({
+        token: '',
+        nextPath: nextPath,
+    })
 
     useEffect(() => {
         setHasError(authError === 'invalid_token')
+        setIsLoading(false)
     }, [authError])
 
+    const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
+        setIsLoading(true)
+        e.preventDefault()
+
+        try {
+            const formDataObj = new FormData()
+            formDataObj.append('token', formData.token)
+            formDataObj.append('nextPath', formData.nextPath)
+
+            await authenticateGuestToken(formDataObj)
+        } catch (error) {
+            console.error('RSVP submission failed:', error)
+            setHasError(true)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
     return (
-        <form action={authenticateGuestToken} className="flex flex-col  w-full">
+        <form className="flex flex-col  w-full">
             <p className="text-6xl mt-4 font-bold moontime mb-4 text-center">
                 Enter Your Token
             </p>
@@ -30,7 +54,11 @@ export default function TokenAccessForm({
                 placeholder="Invitation token"
                 className={`${hasError ? 'border-red-300' : 'border-(--foreground)'} border-2 py-2 px-4 rounded`}
                 autoComplete="off"
-                onChange={() => setHasError(false)}
+                value={formData.token}
+                onChange={(e) => {
+                    setFormData({ ...formData, token: e.target.value })
+                    setHasError(false)
+                }}
                 required
             />
             {hasError && (
@@ -39,12 +67,13 @@ export default function TokenAccessForm({
                     invitation and try again.
                 </p>
             )}
-            <input type="hidden" name="nextPath" value={nextPath} />
             <button
                 type="submit"
-                className="bg-(--foreground) text-(--background) py-2 px-4 rounded mt-10 w-max self-center cursor-pointer"
+                className="bg-(--foreground) text-(--background) py-2 px-4 rounded mt-10 w-max self-center "
+                disabled={loading}
+                onClick={() => handleSubmit}
             >
-                Enter
+                {loading ? 'Entering...' : 'Enter'}
             </button>
         </form>
     )
