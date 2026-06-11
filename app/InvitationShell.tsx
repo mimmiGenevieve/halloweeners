e5103872c69f471c5bbd78cf08d7bc01a121aaa4
mimmiGenevieve <mimmisandgren@gmail.com>
2026-06-11 18:38:34 +0200

    move out into pages

---
+++ app/InvitationShell.tsx
@@ -0,0 +1,46 @@
+import Link from 'next/link'
+
+type InvitationShellProps = {
+    activePage: 'details' | 'rsvp'
+    children: React.ReactNode
+}
+
+export default function InvitationShell({
+    activePage,
+    children,
+}: InvitationShellProps) {
+    return (
+        <div className="flex flex-col flex-1 items-center justify-center bg-linear-to-b from-mauve-900/10 to-mauve-950/90">
+            <main className="flex flex-1 w-full flex-col items-center py-20">
+                <div className="flex w-full flex-col items-center text-shadow-lg bg-(--background)/60 p-[50px] pb-5">
+                    <h1 className="text-9xl font-bold ">The Halloweeners</h1>
+                    <p className="text-2xl mt-4">
+                        An Invitation to the Eternal Night.
+                    </p>
+
+                    <div className="flex flex-row items-center justify-between mt-10 gap-10 text-2xl">
+                        <Link
+                            href="/"
+                            className={`cursor-pointer ${
+                                activePage === 'details' ? 'underline' : ''
+                            }`}
+                        >
+                            Details
+                        </Link>
+                        |
+                        <Link
+                            href="/rsvp"
+                            className={`cursor-pointer ${
+                                activePage === 'rsvp' ? 'underline' : ''
+                            }`}
+                        >
+                            RSVP
+                        </Link>
+                    </div>
+                </div>
+
+                {children}
+            </main>
+        </div>
+    )
+}
