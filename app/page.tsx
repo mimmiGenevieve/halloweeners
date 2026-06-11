dccc0a7b7f3f94f83574a68f7d90ca3cda4caaa2
mimmiGenevieve <mimmisandgren@gmail.com>
2026-06-11 17:49:50 +0200

    add basic layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,107 @@
-import Image from "next/image";
+'use client'
+import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    const [activePage, setActivePage] = useState<'details' | 'rsvp'>('details')
+    return (
+        <div className="flex flex-col flex-1 items-center justify-center ">
+            <main className="flex flex-1 w-full flex-col items-center py-20">
+                <div className="flex w-full flex-col items-center text-shadow-lg bg-(--background)/60 p-[50px] pb-5">
+                    <h1 className="text-9xl font-bold ">The Halloweeners</h1>
+                    <p className="text-2xl mt-4">
+                        An Invitation to the Eternal Night.
+                    </p>
+
+                    <div className="flex flex-row items-center justify-between mt-10 gap-10 text-2xl">
+                        <button
+                            onClick={() => setActivePage('details')}
+                            className={`cursor-pointer
+                                ${activePage === 'details' ? 'underline' : ''} 
+                            `}
+                        >
+                            Details
+                        </button>
+                        |
+                        <button
+                            onClick={() => setActivePage('rsvp')}
+                            className={`cursor-pointer
+                                ${activePage === 'rsvp' ? 'underline' : ''} 
+                            `}
+                        >
+                            RSVP
+                        </button>
+                    </div>
+                </div>
+
+                {activePage === 'details' && (
+                    <div className="flex flex-col  bg-(--background)/60 p-[50px] mt-30 gap-2 text-xl w-200">
+                        <p className="text-7xl mt-4 font-bold moontime mb-10 text-center">
+                            Essential Details for the Night
+                        </p>
+                        <p>
+                            <b>Bring your own elixir of choice</b>, though light
+                            snacks will be provided.
+                        </p>
+                        <p>
+                            Prizes will be bestowed in the following categories:
+                            <b>Best Duo</b>, <b>Best Single</b>, <b>Scariest</b>
+                            , and <b>Most Creative</b>.
+                        </p>
+                        <p>
+                            The gathering begins promptly.{' '}
+                            <b>Tardiness is not advised.</b> Upon your arrival,
+                            call Twoday on the intercom to be granted entry.
+                        </p>
+                        <p className="italic mt-8 text-center">
+                            The night awaits you; let the shadows guide your
+                            way.
+                        </p>
+                    </div>
+                )}
+                {activePage === 'rsvp' && (
+                    <div className="flex flex-col bg-(--background)/60 p-[50px] mt-30 gap-2 text-xl w-200">
+                        <p className="text-7xl mt-4 font-bold moontime mb-5 text-center">
+                            Registration to attend
+                        </p>
+                        <p className="italic text-center mb-8">
+                            The spirits require your answer no later than
+                            October 28th.
+                        </p>
+                        <form>
+                            <input placeholder="Name" />
+                            <input placeholder="Email" />
+                            <div>
+                                Will you be accompanied by another soul?
+                                <i>
+                                    You may bring a companion; be it a friend,
+                                    partner, or even a particularly charming
+                                    houseplant. But only one.
+                                </i>
+                                <label>
+                                    <input type="radio" /> Yes
+                                </label>
+                                <label>
+                                    <input type="radio" /> No
+                                </label>
+                            </div>
+                            <label>
+                                Kindly share the name of your companion
+                            </label>
+                            <input />
+
+                            <label>
+                                The spirits bid your answer to the cipher.
+                                <i>
+                                    If you have solved the cipher, now is the
+                                    time to write the answer.
+                                </i>
+                                <input />
+                            </label>
+                        </form>
+                    </div>
+                )}
+            </main>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    )
 }
